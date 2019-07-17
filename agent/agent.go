4ab7f37e166ff62c5dca79189ba66590301b846b
Kyle Havlovitz <kylehav@gmail.com>
2019-07-17 11:49:54 -0700

    Add agent cachetype for streaming HealthServices

--- agent/agent.go
+++ agent/agent.go
@@ -18,6 +18,8 @@ import (
 	"sync"
 	"time"
 
+	"github.com/hashicorp/consul/agent/consul/stream"
+
 	"google.golang.org/grpc"
 
 	metrics "github.com/armon/go-metrics"
@@ -94,6 +96,7 @@ type delegate interface {
 	RemoveFailedNode(node string) error
 	ResolveToken(secretID string) (acl.Authorizer, error)
 	RPC(method string, args interface{}, reply interface{}) error
+	GRPCConn() (*grpc.ClientConn, error)
 	ACLsEnabled() bool
 	UseLegacyACLs() bool
 	SnapshotRPC(args *structs.SnapshotRequest, in io.Reader, out io.Writer, replyFn structs.SnapshotReplyFn) error
@@ -253,6 +256,9 @@ type Agent struct {
 	// grpcServer is the server instance used currently to serve xDS API for
 	// Envoy.
 	grpcServer *grpc.Server
+
+	// streamClient is the client to use for streaming gRPC endpoints.
+	streamClient stream.ConsulClient
 
 	// tlsConfigurator is the central instance to provide a *tls.Config
 	// based on the current consul configuration.
@@ -428,6 +434,14 @@ func (a *Agent) Start() error {
 	// and that should be hidden in the state syncer implementation.
 	a.State.Delegate = a.delegate
 	a.State.TriggerSyncChanges = a.sync.SyncChanges.Trigger
+
+	// Set up the gRPC client for the cache.
+	conn, err := a.delegate.GRPCConn()
+	if err != nil {
+		return err
+	}
+
+	a.streamClient = stream.NewConsulClient(conn)
 
 	// Register the cache. We do this much later so the delegate is
 	// populated from above.
@@ -3694,14 +3708,14 @@ func (a *Agent) registerCache() {
 		RefreshTimeout: 10 * time.Minute,
 	})
 
-	a.cache.RegisterType(cachetype.HealthServicesName, &cachetype.HealthServices{
-		RPC: a,
-	}, &cache.RegisterOptions{
-		// Maintain a blocking query, retry dropped connections quickly
-		Refresh:        true,
-		RefreshTimer:   0 * time.Second,
-		RefreshTimeout: 10 * time.Minute,
-	})
+	a.cache.RegisterType(cachetype.StreamingHealthServicesName,
+		cachetype.NewStreamingHealthServices(a.streamClient),
+		&cache.RegisterOptions{
+			// Maintain a blocking query, retry dropped connections quickly
+			Refresh:        true,
+			RefreshTimer:   0 * time.Second,
+			RefreshTimeout: 10 * time.Minute,
+		})
 
 	a.cache.RegisterType(cachetype.PreparedQueryName, &cachetype.PreparedQuery{
 		RPC: a,
@@ -3722,8 +3736,7 @@ func (a *Agent) registerCache() {
 	a.cache.RegisterType(cachetype.ResolvedServiceConfigName, &cachetype.ResolvedServiceConfig{
 		RPC: a,
 	}, &cache.RegisterOptions{
-		// Maintain a blocking query, retry dropped connections quickly
-		Refresh:        true,
+		Refresh:        false,
 		RefreshTimer:   0 * time.Second,
 		RefreshTimeout: 10 * time.Minute,
 	})
